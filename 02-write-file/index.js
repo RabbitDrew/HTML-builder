bc27992521a47d21a33ba2b13a57b8f3c548258d
Andrew Sparish <Andrei.sp.93@gmail.com>
2024-01-23 16:35:50 +0300

    fix: fix 02-write-file task

--- 02-write-file/index.js
+++ 02-write-file/index.js
@@ -1,9 +1,21 @@
 const fs = require('fs');
-
-const create = fs.createWriteStream('02-write-file/text.txt');
-create.write('Hello');
-
-const read = fs.createReadStream('02-write-file/text.txt');
-read.on('data', (chank) => {
-  console.log(chank.toString());
+const readline = require('readline')
+const rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout
 });
+
+function quest () {
+  rl.question("Hi bro! what's up?", function(answer) {
+    console.log (answer)
+    fs.appendFile('02-write-file/text.txt', answer + '\n', function(err) {
+      if (err) {
+        console.error(err);
+      } else {
+        console.log("The file was updated!");
+        quest ()
+      }
+    });
+  });
+}
+quest ()
--- 03-files-in-folder/index.js
+++ 03-files-in-folder/index.js
@@ -1,17 +1,3 @@
 let fs = require('fs');
-const path = require('path');
 
-const files = ['data.csv', 'script.js', 'style.css', 'text.txt'];
-for (let i = 0; i < files.length; i++) {
-  fs.readFile(
-    path.join(__dirname, 'secret-folder', files[i]),
-    'utf-8',
-    function (err, data) {
-      if (err) {
-        console.error(err);
-      } else {
-        console.log(data);
-      }
-    },
-  );
-}
+const createReadStream (.)
