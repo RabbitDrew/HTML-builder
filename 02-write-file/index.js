8150e35be3a10872020c89f0d563be3f2c204a7b
RabbitDrew <Andrei.sp.93@gmail.com>
2024-01-24 03:58:31 +0300

    fix: fix 03-files-in-folders

--- 02-write-file/index.js
+++ 02-write-file/index.js
@@ -1,21 +1,21 @@
 const fs = require('fs');
-const readline = require('readline')
+const readline = require('readline');
 const rl = readline.createInterface({
   input: process.stdin,
-  output: process.stdout
+  output: process.stdout,
 });
 
-function quest () {
-  rl.question("Hi bro! what's up?", function(answer) {
-    console.log (answer)
-    fs.appendFile('02-write-file/text.txt', answer + '\n', function(err) {
+function quest() {
+  rl.question("Hi bro! what's up?", function (answer) {
+    console.log(answer);
+    fs.appendFile('02-write-file/text.txt', answer + '\n', function (err) {
       if (err) {
         console.error(err);
       } else {
-        console.log("The file was updated!");
-        quest ()
+        console.log('The file was updated!');
+        quest();
       }
     });
   });
 }
-quest ()
+quest();
--- 03-files-in-folder/index.js
+++ 03-files-in-folder/index.js
@@ -1,3 +1,30 @@
 let fs = require('fs');
+const path = require('path');
 
-const createReadStream (.)
+fs.readdir('03-files-in-folder/secret-folder', function (err, files) {
+  if (err) {
+    console.log(err);
+  } else {
+    getFilesData(files);
+  }
+});
+
+function getFilesData(files) {
+  for (let i = 0; i < files.length; i++) {
+    fs.stat(
+      `03-files-in-folder/secret-folder/${files[i]}`,
+      function (err, stats) {
+        if (err) {
+          console.log(err);
+        } else {
+          const { name, ext } = path.parse(files[i]);
+          const { size } = stats;
+
+          console.log(`file name: ${name}`);
+          console.log(`file extension: ${ext}`);
+          console.log(`file size: ${size} байт`);
+        }
+      },
+    );
+  }
+}
