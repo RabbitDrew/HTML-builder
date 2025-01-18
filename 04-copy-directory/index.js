d256df1e7840afc3d0bb2acfcb75d03f653280b3
Andrew <andrei.sp.93@gmail.com>
2025-01-18 22:15:11 +0300

    feat: finish 04-copy-dirictory

---
+++ 04-copy-directory/index.js
@@ -0,0 +1,35 @@
+const fs = require('fs');
+const path = require('path');
+const dirPath = path.join(__dirname, 'files-copy');
+const sourcePath = path.join(__dirname, 'files');
+
+fs.mkdir(dirPath, { recursive: true }, (err) => {
+  if (err) {
+    console.log(err);
+  } else {
+    copyDir();
+  }
+});
+
+const copyDir = () => {
+  fs.readdir(sourcePath, { withFileTypes: true }, (err, files) => {
+    if (err) {
+      console.log(err);
+      return;
+    } else {
+      files.forEach((file) => {
+        const currFilePath = path.join(file.path, file.name);
+        const destPath = path.join(dirPath, file.name);
+        fs.copyFile(currFilePath, destPath, (err) => {
+          if (err) {
+            console.log(err);
+          } else {
+            console.log('copied', file.name);
+          }
+        });
+      });
+    }
+  });
+};
+
+
