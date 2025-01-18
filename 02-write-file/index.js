506c9cfa16535a0e39e3256bf43a0b0e5a54f32c
Andrew <andrei.sp.93@gmail.com>
2025-01-18 13:24:56 +0300

    feat: finish 02-write-file

--- 02-write-file/index.js
+++ 02-write-file/index.js
@@ -2,40 +2,10 @@ const fs = require('fs');
 const path = require('path');
 const readLine = require('readline');
 const filePath = path.join(__dirname, 'text.txt');
-//check wheather the file exist or not
 const rl = readLine.createInterface({
   input: process.stdin,
   output: process.stdout,
 });
-
-const fileCreator = (filePath, content) => {
-  fs.readFile(filePath, 'utf-8', (err, data) => {
-    if (err) {
-      writeFile(filePath, content);
-    } else {
-      appendFile(filePath, content);
-    }
-  });
-
-  const writeFile = (filePath, content) => {
-    fs.writeFile(filePath, content + '\n', (err) => {
-      if (err) {
-        console.log(err);
-      } else {
-        console.log('file is written');
-      }
-    });
-  };
-
-  const appendFile = (filePath, content) => {
-    fs.appendFile(filePath, content + '\n', (err) => {
-      if (err) {
-        console.log(err);
-      } else {
-      }
-    });
-  };
-};
 
 const getInput = function () {
   rl.question('Enter the message: ', (input) => {
@@ -44,7 +14,10 @@ const getInput = function () {
       rl.close();
       process.exit();
     } else {
-      fileCreator(filePath, input);
+      const writeStream = fs.createWriteStream (filePath, { flags: 'a', encoding: 'utf-8' }) 
+      writeStream.on('open', () => {})
+      writeStream.write(input + `\n`)
+      writeStream.end();
       getInput();
     }
   });
